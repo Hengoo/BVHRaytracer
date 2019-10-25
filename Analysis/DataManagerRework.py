eb22787b2bcdee0aaaf763ba8b52982855418c23
Hengo <leitner.andi@googlemail.com>
2019-10-25 17:14:43 +0200

    finished data manger for now. Adjusted tex doc to new names

--- Analysis/DataManagerRework.py
+++ Analysis/DataManagerRework.py
@@ -3,8 +3,10 @@ from os import path
 
 class everything:
 	def __init__(self):
-		self.names = ["shiftHappens", "sponza", "rungholt", "erato"]
-		self.prefix = "SaveSorted"
+		#self.names = ["shiftHappens", "sponza", "rungholt", "erato"]
+		self.names = ["shiftHappens", "erato", "sponza"]
+		self.prefix = ""
+		self.outputPrefix = "Sorted"
 
 		# -1 for all, id otherwise (starting with 0)
 		self.singeIdOverride = -1
@@ -25,8 +27,8 @@ class everything:
 			"secondary intersections leaf:"
 			]
 		self.variableOutputNames = [
-			"primaryNodeIntersections:",
-			"primaryLeafIntersections:",
+			"primaryNodeIntersections",
+			"primaryLeafIntersections",
 			"secondaryNodeIntersections",
 			"secondaryLeafIntersections"
 			]
@@ -62,8 +64,8 @@ class everything:
 		#now for each type of variable: array[sceneId][nameId] -> maxValue
 		self.variablesMax = [[0 for i in self.variableNames]for i in self.names]
 		self.variablesMin = [[2000000 for i in self.variableNames]for i in self.names]
-		self.costMetricsMax = [[0 for i in range(len(self.costMetricNames)*2)]for i in self.names]
-		self.costMetricsMin = [[2000000 for i in range(len(self.costMetricNames)*2)]for i in self.names]
+		self.costMetricsMax = [[0 for i in self.costMetricNames]for i in self.names]
+		self.costMetricsMin = [[2000000 for i in self.costMetricNames]for i in self.names]
 		self.normalizedVariablesMax = [[0 for i in self.normalizedVariableNames]for i in self.names]
 		self.normalizedVariablesMin = [[2000000 for i in self.normalizedVariableNames] for i in self.names]
 		#could also store min and max id (b and l)
@@ -71,16 +73,26 @@ class everything:
 		# for each type of variable: array[nameId] -> value of current file.
 		# (so we loop over a file, fill this array and at the end print one line in the summary)
 		self.variables = [[0 for i in self.variableNames]for i in self.names]
-		self.costMetrics = [[0 for i in range(len(self.costMetricNames)*2)]for i in self.names]
+		self.costMetrics = [[0 for i in self.costMetricNames]for i in self.names]
 		self.normalizedVariables = [[0 for i in self.normalizedVariableNames]for i in self.names]
 
 	def run(self):
 		# now loop over all scenes to do the single scene file (and collect min max)
 		# then loop over all and get averages
 
+		firstLine = "branchFactor, leafSize"
+		for name in self.variableOutputNames:
+			firstLine += ", " + name
+		for name in self.costMetricOutputNames:
+			firstLine += ", " + name
+		for name in self.normalizedVariableOutputNames:
+			firstLine += ", " + name
+
 		for i in range(len(self.names)):
-			fResult = open(self.names[i] + self.prefix + "TableSorted.txt", "w+")
+			fResult = open(self.names[i] + self.prefix + "Table" + self.outputPrefix + ".txt", "w+")
 			# write the first line in the table (the one with the variable names)
+
+			fResult.write(firstLine + "\n")
 
 			for b in range(self.maxBranchingFactor - 1):
 				#one empty line after each branching factor
@@ -119,22 +131,108 @@ class everything:
 								for v in range(len(self.normalizedVariableNames)):
 									self.gatherNormalizedVariables(i, v, x)
 					else:
-						print("Was not able to open file: " + fileName)
+						print("Was not able to open file: " + fileName2)
 					
 					#TODO: need to gather min and max of cost functions
+					for v in range(len(self.costMetricNames)):
+						self.costMetricsMin[i][v] = min(self.costMetrics[i][v], self.costMetricsMin[i][v])
+						self.costMetricsMax[i][v] = max(self.costMetrics[i][v], self.costMetricsMax[i][v])
 
 					#write file:
 					res = self.getLine(i, branch, leaf)
 					fResult.write(res)
 
+		#now loop over b and l again and write average file:
+		fResult = open("AverageTable" + self.outputPrefix + ".txt", "w+")
+		# write the first line in the table (the one with the variable names)
+		fResult.write(firstLine + "\n")
+		for b in range(self.maxBranchingFactor - 1):
+			#one empty line after each branching factor
+			fResult.write("\n")
+			for l in range(self.maxLeafSize):
+				branch = b + 2
+				leaf = l + 1
+				#loop over scenes
+				for i in range(len(self.names)):
+					#i just test both files for all varialbe names
+					fileName = self.names[i] + self.prefix + "/" + self.names[i] + "_b" + str(branch) + "_l" + str(leaf) + "_Info.txt"
+					fileName2 = self.names[i] + self.prefix + "/" + self.names[i] + "_b" + str(branch) + "_l" + str(leaf) + "_BVHInfo.txt"
+					if (path.exists(fileName)):
+						#open file and read important values
+						f = open(fileName, "r")
+						if f.mode == 'r':
+							for x in f:
+								#now loop over the different variables and call somefunctions to hande the rest
+								for v in range(len(self.variableNames)):
+									self.gatherVariables(i,v,x)
+								for v in range(len(self.costMetricNames)):
+									self.gatherCostVariables(i,v,x)
+								for v in range(len(self.normalizedVariableNames)):
+									self.gatherNormalizedVariables(i, v, x)
+					else:
+						print("Was not able to open file: " + fileName)
+					if (path.exists(fileName2)):
+						#open file and read important values
+						f = open(fileName2, "r")
+						if f.mode == 'r':
+							for x in f:
+								#now loop over the different variables and call somefunctions to hande the rest
+								for v in range(len(self.variableNames)):
+									self.gatherVariables(i,v,x)
+								for v in range(len(self.costMetricNames)):
+									self.gatherCostVariables(i,v,x)
+								for v in range(len(self.normalizedVariableNames)):
+									self.gatherNormalizedVariables(i, v, x)
+					else:
+						print("Was not able to open file: " + fileName2)
+
+				#write file:
+				res = self.getAverageLine(branch, leaf)
+				fResult.write(res)
+
+		
+
+	def getAverageLine(self, branch, leaf):
+		sceneNumber = len(self.names)
+		result = str(branch) + ", " + str(leaf)
+		for valueId in range(len(self.variableNames)):
+			value = 0
+			for i in range(len(self.names)):
+				value += self.variables[i][valueId] / self.variablesMax[i][valueId]
+				self.variables[i][valueId] = 0
+			result += ", " + str(value / sceneNumber)
+
+		for valueId in range(len(self.costMetricNames)):
+			value = 0
+			for i in range(len(self.names)):
+				value += self.costMetrics[i][valueId] / self.costMetricsMax[i][valueId]
+				self.costMetrics[i][valueId] = 0
+			result += ", " + str(value / sceneNumber)
+
+		for valueId in range(len(self.normalizedVariableNames)):
+			value = 0
+			for i in range(len(self.names)):
+				value += self.normalizedVariables[i][valueId]
+				self.normalizedVariables[i][valueId] = 0
+			result += ", " + str(value / sceneNumber)
+
+		return result + "\n"
+
+	#returns the line and resets the current values
 	def getLine(self, sceneId, branch, leaf):
 		result = str(branch) + ", " + str(leaf)
-		for value in self.variables[sceneId]:
+		for id in range(len(self.variableNames)):
+			value = self.variables[sceneId][id]
 			result += ", " + str(value)
-		for value in self.costMetrics[sceneId]:
+			self.variables[sceneId][id] = 0
+		for id in range(len(self.costMetricNames)):
+			value = self.costMetrics[sceneId][id]
 			result += ", " + str(value)
-		for value in self.normalizedVariables[sceneId]:
+			self.costMetrics[sceneId][id] = 0
+		for id in range(len(self.normalizedVariableNames)):
+			value = self.normalizedVariables[sceneId][id]
 			result += ", " + str(value)
+			self.normalizedVariables[sceneId][id] = 0
 		return result + "\n"
 
 	def gatherVariables(self, sceneId, variableId, string):
@@ -144,7 +242,7 @@ class everything:
 					value = float(t)
 					self.variablesMax[sceneId][variableId] = max(self.variablesMax[sceneId][variableId], value)
 					self.variablesMin[sceneId][variableId] = min(self.variablesMin[sceneId][variableId], value)
-					self.variables[sceneId][variableId] = value
+					self.variables[sceneId][variableId] += value
 					#only take first value (second one might be average)
 					break
 				except ValueError:
@@ -155,7 +253,7 @@ class everything:
 			for t in string.split():
 				try:
 					value = float(t)
-					self.costMetrics[sceneId][variableId * 2] = value * self.nodeCostFactor
+					self.costMetrics[sceneId][variableId] += value * self.nodeCostFactor
 					#only take first value (second one might be average)
 					break
 				except ValueError:
@@ -164,7 +262,7 @@ class everything:
 			for t in string.split():
 				try:
 					value = float(t)
-					self.costMetrics[sceneId][variableId * 2 + 1] = value * self.leafCostFactor
+					self.costMetrics[sceneId][variableId] += value * self.leafCostFactor
 					#only take first value (second one might be average)
 					break
 				except ValueError:
@@ -177,7 +275,7 @@ class everything:
 					value = float(t)
 					self.normalizedVariablesMax[sceneId][variableId] = max(self.normalizedVariablesMax[sceneId][variableId], value)
 					self.normalizedVariablesMin[sceneId][variableId] = min(self.normalizedVariablesMin[sceneId][variableId], value)
-					self.normalizedVariables[sceneId][variableId] = value
+					self.normalizedVariables[sceneId][variableId] += value
 					#only take first value (second one might be average)
 					break
 				except ValueError:
