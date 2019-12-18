39bb70bc4a903b2e49e5648578d2ffa2bfa2db45
Hengo <leitner.andi@googlemail.com>
2019-12-18 14:02:49 +0100

    Result of looking at assembler for one hour.

--- src/ISPC/IspcTests/testCompile.cmd
+++ src/ISPC/IspcTests/testCompile.cmd
@@ -1,9 +1,11 @@
-ispc constAll.ispc -o ISPCBuild/constAll.s --target=sse4-i32x4 --emit-asm
-ispc paramAll.ispc -o ISPCBuild/paramAll.s --target=sse4-i32x4 --emit-asm
-ispc paramLoop.ispc -o ISPCBuild/paramLoop.s --target=sse4-i32x4 --emit-asm
-ispc paramAllV2.ispc -o ISPCBuild/paramAllV2.s --target=sse4-i32x4 --emit-asm
-ispc paramLoopV2.ispc -o ISPCBuild/paramLoopV2.s --target=sse4-i32x4 --emit-asm
-ispc paramMemory.ispc -o ISPCBuild/paramMemory.s --target=sse4-i32x4 --emit-asm
+set OPT=--emit-asm --target=sse4-i32x4 --x86-asm-syntax=intel
+
+ispc constAll.ispc -o ISPCBuild/constAll.s %OPT%
+ispc paramAll.ispc -o ISPCBuild/paramAll.s %OPT%
+ispc paramLoop.ispc -o ISPCBuild/paramLoop.s %OPT%
+ispc paramAllV2.ispc -o ISPCBuild/paramAllV2.s %OPT%
+ispc paramLoopV2.ispc -o ISPCBuild/paramLoopV2.s %OPT%
+ispc paramMemory.ispc -o ISPCBuild/paramMemory.s %OPT%
 
 
 :: --target=sse4-i32x4
